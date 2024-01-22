0d4273f8daeab94ea658086fa4849d2245d7f20b
sunggyupaik <melon9751@naver.com>
2024-01-22 16:15:31 +0900

    [feat] NewsView에 동적 라우팅을 추가하라
    라우터 설정에 `/user/:id`를 추가하여 동적으로 라우팅되도록 합니다.
    view 화면에서는 `<router-link :to="`/user/${item.user}`"> 를 활용하여
    사용자 명을 기준으로 동적 라우팅을 합니다.

--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -32,7 +32,7 @@ export const router = new VueRouter({
             component: ItemView,   
         },
         {
-            path: '/user',
+            path: '/user/:id',
             component: UserView,   
         }
     ]
