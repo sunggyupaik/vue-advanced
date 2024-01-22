fb89dbd9838422b08a592006d9d459e4045dee58
sunggyupaik <melon9751@naver.com>
2024-01-22 11:25:07 +0900

    [feat] store를 활용해 NewsView를 개선하라
    state, mutations, actions를 활용해 NewsView 화면을 개선합니다.
    store/index.js에서 상태를 관리하며 NewsView에서는 $store로 접근해
    조회 결과를 화면에 뿌려줍니다.

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,25 +1,19 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user.id">{{ user }}</div>
+    <div v-for="user in this.$store.state.news" :key="user.id">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index';
-
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    fetchNewsList()
-      .then(response => {
-        this.users = response.data;
-        console.log(this.users);
-      })
-      .catch(error => console.log(error));
+    this.$store.dispatch('FETCH_NEWS');
+    // fetchNewsList()
+    //   .then(response => {
+    //     this.users = response.data;
+    //     console.log(this.users);
+    //   })
+    //   .catch(error => console.log(error));
   }
 }
 </script>
