90113b086f1f7b9438e2e21014e364d452f61e9e
sunggyupaik <melon9751@naver.com>
2024-01-22 14:20:27 +0900

    [feat] actions와 mutations를 모듈화하라
    index.js에 state, getters, actions, mutations가 모두 정의되어 있습니다.
    코드 양이 늘어나면 복잡해지므로 actions와 mutations를 분리합니다.

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList } from '../api/index.js';
-import { fetchJobsList } from '../api/index.js';
-import { fetchAskList } from '../api/index.js';
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
@@ -17,47 +16,6 @@ export const store = new Vuex.Store({
             return state.ask;
         }
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        }
-    },
-    actions: {
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => {
-                    console.log(response.data);
-                    context.commit('SET_NEWS', response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-        FETCH_JOBS({ commit }) {
-            fetchJobsList()
-                .then(({ data }) => {
-                    console.log(data);
-                    commit('SET_JOBS', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-        FETCH_ASK({ commit }) {
-            fetchAskList()
-                .then(({ data }) => {
-                    console.log(data);
-                    commit('SET_ASK', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-    }
+    mutations: mutations,
+    actions: actions
 });
