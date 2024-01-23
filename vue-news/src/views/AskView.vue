8d8cf5f4411fae6a1b9ac4aa62a9d6c6fee4de02
sunggyupaik <melon9751@naver.com>
2024-01-23 17:27:24 +0900

    [feat] template과 v-if로 일부 화면을 분기하라
    `template` 속성과 `v-if`, `v-else`를 활용해 화면을 분기합니다.
    서버에서 받아온 데이터 중에 ask, news에만 있는 경우와
    jobs에만 있는 경우 2가지가 있습니다.
    
    template에 분기문을 이용하여 데이터 존재 요뷰에 따라 화면에
    다른 데이터가 표시되도록 개선합니다.

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -16,26 +16,3 @@ export default {
 
 <style>
 </style>
-
-
-  <!-- <div>
-    <ul class="news-list">
-      <li v-for="item in fetchedAsk" :key="item.id" class="post">
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <div>
-          <router-link v-bind:to="`item/${item.id}`">
-            {{ item.title }}
-          </router-link>
-          <div>
-            <small class="link-text">
-              {{ item.time_ago }} 
-              by 
-              <router-link :to="`/item/${item.id}`" class="link-text">{{ item.user }}</router-link>
-            </small>
-          </div>
-        </div>
-      </li>
-    </ul>
-  </div> -->
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -16,28 +16,3 @@ export default {
 
 <style>
 </style>
-
-<!-- <template>
-  <div>
-    <ul class="news-list">
-      <li v-for="job in this.$store.state.jobs" :key="job.id" class="post">
-        <div class="points">
-          {{ 0 }}
-        </div>
-        <div>
-          <p class="news-title">
-            <a :href="job.url">{{ job.title }}</a>
-          </p>
-          <small class="link-text">
-            {{ job.time_ago }} 
-            by 
-            <!-- <router-link :to="`/user/${job.user}`" class="link-text">{{ job.domain }}</router-link> -->
-            <a :href="`${job.url}`">
-              {{ job.url }}
-            </a>
-          </small>
-        </div>
-      </li>
-    </ul>
-  </div>
-</template> -->
