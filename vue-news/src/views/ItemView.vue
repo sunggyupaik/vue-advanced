fb94fde9cf2ae6814d559d9225903989ba67fa8d
sunggyupaik <melon9751@naver.com>
2024-01-23 10:02:46 +0900

    [feat] ItemView에 상세정보를 추가하라
    작성자, 작성시간, 제목, 내용 등을 추가합니다.
    `mapGetters`를 사용해 item을 조회하여 `$store`로
    직접 접근할 때보다 가독성을 높입니다.
    
    `v-html`을 사용하여 글 내용이 html 태그가 적용된 상태로
    화면에 그려지도록 합니다.
    `v-router`를 사용해 작성자를 클릭하면 UserView로 전환합니다.

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,14 +1,50 @@
 <template>
   <div>
+    <section>
+      <div>
+        <div class="user-container">
+          <div>
+            <i class="fas fa-user"></i>
+          </div>
+          <div class="user-description">
+            <!-- 라우터 링크 item user 넣기 -->
+            <router-link :to="`/user/${fetchedItem.user}`">
+              {{ fetchedItem.user }}
+            </router-link>
+            <div class="time">
+            <!-- item time_ago 넣기 -->
+            {{ fetchedItem.time_ago }}
+          </div>
+          </div>
+        </div>
+      </div>
+      <h2>
+          <!-- item 제목 넣기 -->
+          {{ fetchedItem.title }}
+      </h2>
+    </section>
+    <section>
+      <!-- item 내용 넣기 -->
+      <div v-html="fetchedItem.content"></div>
+    </section>
+  </div>
+  <!-- <div>
     <p>{{ this.$store.state.item.title }}</p>
     <div>
       {{ this.$store.state.item.content }}
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+  computed: {
+    ...mapGetters([
+      'fetchedItem'
+    ]),
+  },
   created() {
     const itemId = this.$route.params.id;
     this.$store.dispatch('FETCH_ITEM', itemId);
@@ -16,6 +52,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
 
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user-description {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
 </style>
