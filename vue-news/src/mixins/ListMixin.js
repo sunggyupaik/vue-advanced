035ccc35c8e4f437690dce7184fd3c116b5ff8bb
sunggyupaik <melon9751@naver.com>
2024-01-25 23:53:06 +0900

    [feat] mixin을 활용해 ListItem을 개선하라
    mixin을 사용해, 공통되는 API 호출 사용을 개선합니다.
    hoc와 다른 점은 계층이, AskView, NewsView, JobView
    바로 하위에 ListItem 컴포넌트가 위치합니다.
    
    반복되는 코드를 쉽게 재사용할 수 있습니다.

---
+++ vue-news/src/mixins/ListMixin.js
@@ -0,0 +1,17 @@
+import bus from '../utils/bus.js';
+
+export default {
+    created() {
+    bus.$emit('start:spinner');
+    setTimeout(() => {
+      this.$store.dispatch('FETCH_LIST', this.$route.name)
+        .then(() => {
+          console.log('fetched');
+          bus.$emit('end:spinner');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 3000);
+  }
+}
