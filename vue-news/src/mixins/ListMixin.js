691b7a2164c5b16426dd35ffd15cc6fb95aba91c
sunggyupaik <melon9751@naver.com>
2024-01-26 11:24:29 +0900

    [feat] 라우터 네비게이션 가드로 개선하라
    라우터 네비게이션으로, 리스트 조회를 개선합니다.
    라우터에 `beforeEnter(to, from, next)` 를 사용해
    화면이 이동하기 이전에 미리 조회하도록 합니다.
    
    각 컴포넌트에서 mixin을 활용하여 개선하였는데,
    router쪽에서 한번만 코드를 작성하면 됩니다.

--- vue-news/src/mixins/ListMixin.js
+++ vue-news/src/mixins/ListMixin.js
@@ -1,7 +1,7 @@
 import bus from '../utils/bus.js';
 
 export default {
-    created() {
+  created() {
     bus.$emit('start:spinner');
     setTimeout(() => {
       this.$store.dispatch('FETCH_LIST', this.$route.name)
--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -6,6 +6,8 @@ import JobsView from '../views/JobsView.vue';
 import ItemView from '../views/ItemView.vue';
 import UserView from '../views/UserView.vue';
 import createListView from "../views/CreateListView.js";
+import bus from '../utils/bus.js';
+import { store } from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -21,18 +23,60 @@ export const router = new VueRouter({
             name: 'news',
             component: NewsView,   
             // component: createListView('NewsView'),
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                setTimeout(() => {
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                    console.log('fetched');
+                    bus.$emit('end:spinner');
+                    next();
+                    })
+                    .catch((error) => {
+                    console.log(error);
+                    });
+                }, 3000);
+            }
         },
         {
             path: '/ask',
             name: 'ask',
             component: AskView,
             // component: createListView('AskView'),
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                setTimeout(() => {
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                    console.log('fetched');
+                    bus.$emit('end:spinner');
+                    next();
+                    })
+                    .catch((error) => {
+                    console.log(error);
+                    });
+                }, 3000);
+            }
         },
         {
             path: '/jobs',
             name: 'jobs',
             component: JobsView,   
             // component: createListView('JobsView'),
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                setTimeout(() => {
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                    console.log('fetched');
+                    bus.$emit('end:spinner');
+                    next();
+                    })
+                    .catch((error) => {
+                    console.log(error);
+                    });
+                }, 3000);
+            }
         },
         {
             path: '/item/:id',
